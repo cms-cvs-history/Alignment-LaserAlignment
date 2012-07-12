07a737809f54a5b2770bc1bc8c40a3f57c9c5a22
cmsbuild <>
2012-07-12 14:56:57 +0000

    This commit was manufactured by cvs2git to create tag 'Alignment- LaserAlignment-V03-09-01-04'.
    Sprout from master 2011-12-22 18:06:57 UTC Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch> 'remove instance'
    Cherrypick from master 2010-09-29 09:52:51 UTC Bruno Wittmer <sha1-a68e14ffc3191775a6893edb5ff9bc223a940403@cern.ch> 'Fixes for CMSSW_3_9_x':
        interface/LASGlobalData.h
        plugins/RawDataConverter.cc
        plugins/RawDataConverter.h
        python/LaserAlignmentDefs_cff.py
        python/RawDataConverter_cfi.py
        test/LaserAlignmentEventFilter_test.py
        test/RawDataConverter_full_chain.py
        test/t0ProducerStandalone_LASFilter.py
    Cherrypick from master 2012-07-12 14:56:56 UTC Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch> 'Remove duplicate extra constants which come from GeomDet.':
        plugins/LaserAlignment.cc
        plugins/LaserAlignment.h
    Cherrypick from master 2011-02-19 15:48:28 UTC Andreas Mussgiller <sha1-221a492635cb55c4151f2a5361bd86aeae15e99c@cern.ch> 'removed left over cout statements':
        plugins/LaserAlignmentEventFilter.cc
    Delete:
        interface/LASGlobalDataLoop.h
        python/LaserAlignmentHLT_cfi.py
        scripts/master_script.sh
        scripts/merge_RunXXXXXX_EvYYYYYY.py
        scripts/merge_RunXXXXXX_EvYYYYYY_part1.py
        scripts/merge_RunXXXXXX_EvYYYYYY_part2.py
        scripts/run_job_LAS_RunXXXXXX_EvYYYYYY.sh
        scripts/run_on_LSF_LAS_RunXXXXXX_EvYYYYYY.sh
        scripts/t0ProducerStandalone_part1.py
        scripts/t0ProducerStandalone_part2_Run.py
        scripts/t0ProducerStandalone_part3_RunXXXXXX_EvYYYYYY.py
        scripts/write_bjobs_LAS_RunXXXXXX_EvYYYYYY.sh
        scripts/write_merge_RunXXXXXX_EvYYYYYY.sh
        scripts/write_python_config_files_LAS_RunXXXXXX_EvYYYYYY.sh
        src/LASGlobalDataLoop.cc
        test/LaserAlignmentHLT.py
        test/RawDataConverter_test.py

--- plugins/LaserAlignment.cc
+++ plugins/LaserAlignment.cc
@@ -1,8 +1,8 @@
 /** \file LaserAlignment.cc
  *  LAS reconstruction module
  *
- *  $Date: 2011/09/16 07:45:48 $
- *  $Revision: 1.41 $
+ *  $Date: 2012/06/13 16:23:31 $
+ *  $Revision: 1.44 $
  *  \author Maarten Thomas
  *  \author Jan Olzem
  */
@@ -39,7 +39,9 @@ LaserAlignment::LaserAlignment( edm::ParameterSet const& theConf ) :
   theAlignableTracker(),
   theAlignRecordName( "TrackerAlignmentRcd" ),
   theErrorRecordName( "TrackerAlignmentErrorRcd" ),
-  firstEvent_(true) {
+  firstEvent_(true),
+  theParameterSet( theConf )
+{
 
 
   std::cout << std::endl;
@@ -63,7 +65,6 @@ LaserAlignment::LaserAlignment( edm::ParameterSet const& theConf ) :
 	    << "\n    ----------------------------------------------- ----------"
 	    << (theSetNominalStrips?"\n    Set strips to nominal       =  true":"\n")
 	    << "\n=============================================================" << std::endl;
-
 
   // tell about masked modules
   if( theMaskTecModules.size() ) {
@@ -285,7 +286,12 @@ void LaserAlignment::produce(edm::Event& theEvent, edm::EventSetup const& theSet
       edm::ESHandle<GeometricDet> theGeometricDet;
       theSetup.get<IdealGeometryRecord>().get(theGeometricDet);
       TrackerGeomBuilderFromGeometricDet trackerBuilder;
-      TrackerGeometry* theRefTracker = trackerBuilder.build(&*theGeometricDet);
+      const edm::ParameterSet tkGeomConsts( theParameterSet.getParameter<edm::ParameterSet>( "trackerGeometryConstants" ));
+      TrackerGeometry* theRefTracker = trackerBuilder.build(&*theGeometricDet,
+							    tkGeomConsts.getParameter<bool>("upgradeGeometry"),
+							    tkGeomConsts.getParameter<int>( "BIG_PIX_PER_ROC_X" ),
+							    tkGeomConsts.getParameter<int>( "BIG_PIX_PER_ROC_Y" ));
+      
       theAlignableTracker = new AlignableTracker(&(*theRefTracker));
     }
     else {
